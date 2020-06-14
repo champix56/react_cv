2e098cbdf49f49eda3516313c79c5f1c13d70982
alex <desorbaix@free.fr>
2020-06-14 23:40:12 +0200

    correction button

--- src/App/component/Button/Button.js
+++ src/App/component/Button/Button.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Button.module.css';
-
-const Button = (props) => (
-  <div className={styles.Button} data-testid="Button">
+import ReactDOM from 'react-dom';
+const Button = (props) => {
+  onclick=(evt)=>{
+    props.onclick&& props.onclick(evt);
+  }
+  return (
+  <div className={styles.Button} data-testid="Button" onClick={onclick}>
   <div className={styles.content}>
     {props.text}
     </div>
   </div>
-);
+);}
 
-Button.propTypes = {text:PropTypes.string};
+Button.propTypes = {text:PropTypes.string,onclick:PropTypes.func};
 
 Button.defaultProps = {text:"defaultName"};
 
