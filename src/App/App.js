0ea6cd213884ea83418430fb851688dab1c34e53
alex <desorbaix@free.fr>
2020-06-14 21:46:04 +0200

    model d'input text basic toujours le meme probleme le parent ne possede pas d'etat

--- src/App/App.js
+++ src/App/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Button from './component/Button/Button'
 import './App.css';
 import Slider from './component/Slider/Slider';
+import InputText from './component/InputText/InputText';
 
 function App(props) {
   var valueState=true;
@@ -12,6 +13,7 @@ function App(props) {
   return (
     <div className="App">Mon App React<br/> 
     <Slider value={valueState} onClick={sliderToggle}/><br/>{valueState.toString()}
+      <InputText></InputText>
       <Button text="Ok"></Button><Button text="Cancel"></Button>
    </div>
  );
