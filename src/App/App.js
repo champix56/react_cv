db4ec8e7feeafd84fc6c9fbc9e693003ad3f12a8
alex <desorbaix@free.fr>
2020-06-15 01:10:37 +0200

    suppr dead import

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Images from './component/Images/Images';
-import Meme from './component/Meme/Meme';
+
 let images = [{ "id": 1, "url": "img/plonge.jpg" }, { "id": 2, "url": "img/homer1.gif" }, { "id": 3, "url": "img/futurama1.jpg" }, { "id": 4, "url": "img/farnswoth.jpg" }, { "id": 5, "url": "img/fry1.jpg" }, { "id": 6, "url": "img/trololo.jpg" }];
 let meme = { "id": 1, "imageId": 1, "ext": [{ "x": 0, "y": 0, "value": "dev react" }], "image": { "id": 1, "url": "img/plonge.jpg" } };
 function App(props) {
